df6fa6d7ce8c1f77a835c093d1b97c2de733776a
Unknown <jason.wilmans@haw-hamburg.de>
2018-01-01 23:55:26 +0100

    Added box shape (two lines wrong yet)

--- Implementation/DebugDrawer/DebugDrawer.Game/SimpleShapesExample.cs
+++ Implementation/DebugDrawer/DebugDrawer.Game/SimpleShapesExample.cs
@@ -11,6 +11,7 @@ namespace DebugDrawer
         private DebugDrawerSystem _debugDrawer;
         private Line _line2;
         private Line _line1;
+        private Box _box;
 
         public override void Start()
         {
@@ -21,13 +22,17 @@ namespace DebugDrawer
             _debugDrawer.Add(_line1);
             _line2 = new Line(new Vector3(1, 1, 0), new Vector3(1, 1, 1), Color.Red);
             _debugDrawer.Add(_line2);
+            _box = new Box(new Vector3(2.5f, 2, 0), Vector3.One, Color.Yellow);
+            _debugDrawer.Add(_box);
         }
 
         public override void Update()
         {
-            var delta = Vector3.UnitY + new Vector3(0, 0.5f, 0) * (float)Math.Sin(Game.PlayTime.TotalTime.TotalSeconds / 5);
+            float sin = (float) Math.Sin(Game.PlayTime.TotalTime.TotalSeconds);
+            var delta = Vector3.UnitY + new Vector3(0, 0.5f, 0) * sin / 5;
             _line1.End = delta;
             _line2.Start = delta;
+            _box.Scale = Vector3.One * sin;
         }
     }
 }
--- Implementation/DebugDrawer/Xenko.DebugDrawer/Shapes/Box.cs
+++ Implementation/DebugDrawer/Xenko.DebugDrawer/Shapes/Box.cs
@@ -1,9 +1,100 @@
-﻿using SiliconStudio.Core.Mathematics;
+﻿using System.Collections.Generic;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Engine;
 
 namespace Xenko.DebugDrawer.Shapes
 {
-    public class Box
+    public class Box : AShape
     {
-        public Vector3 Position { get; set; }
+        public Vector3 Position
+        {
+            get => _transform.Position;
+            set
+            {
+                _transform.Position = value;
+                CalculateLines();
+            }
+        }
+
+        public Vector3 Scale
+        {
+            get => _transform.Scale;
+            set
+            {
+                _transform.Scale = value;
+                CalculateLines();
+            }
+        }
+
+        private readonly Line[] _lines;
+        private readonly TransformComponent _transform;
+
+        public override IEnumerable<Line> Lines => _lines;
+
+        public Box(Color color) : this(Vector3.Zero, Vector3.One, color)
+        {
+        }
+
+        public Box(Vector3 position, Vector3 scale, Color color) : base(color)
+        {
+            _transform = new TransformComponent();
+            _lines = new Line[12];
+
+            Position = position;
+            Scale = scale;
+
+            CalculateLines();
+        }
+
+        private void CalculateLines()
+        {
+            float xHalf = Scale.X / 2;
+            float yHalf = Scale.Y / 2;
+            float zHalf = Scale.Z / 2;
+
+            // bottom
+            _lines[0] = new Line(
+                new Vector3(_transform.Position.X - xHalf, _transform.Position.X - yHalf, _transform.Position.X - zHalf),
+                new Vector3(_transform.Position.X - xHalf, _transform.Position.X - yHalf, _transform.Position.X + zHalf), Color);
+            _lines[1] = new Line(
+                new Vector3(_transform.Position.X - xHalf, _transform.Position.X - yHalf, _transform.Position.X + zHalf),
+                new Vector3(_transform.Position.X + xHalf, _transform.Position.X - yHalf, _transform.Position.X + zHalf), Color);
+            _lines[2] = new Line(
+                new Vector3(_transform.Position.X + xHalf, _transform.Position.X - yHalf, _transform.Position.X + zHalf),
+                new Vector3(_transform.Position.X + xHalf, _transform.Position.X - yHalf, _transform.Position.X - zHalf), Color);
+            _lines[3] = new Line(
+                new Vector3(_transform.Position.X + xHalf, _transform.Position.X - yHalf, _transform.Position.X - zHalf),
+                new Vector3(_transform.Position.X - xHalf, _transform.Position.X - yHalf, _transform.Position.X - zHalf), Color);
+
+            // top
+            _lines[4] = new Line(
+                new Vector3(_transform.Position.X - xHalf, _transform.Position.X + yHalf, _transform.Position.X - zHalf),
+                new Vector3(_transform.Position.X - xHalf, _transform.Position.X + yHalf, _transform.Position.X + zHalf), Color);
+            _lines[5] = new Line(
+                new Vector3(_transform.Position.X - xHalf, _transform.Position.X + yHalf, _transform.Position.X + zHalf),
+                new Vector3(_transform.Position.X + xHalf, _transform.Position.X + yHalf, _transform.Position.X + zHalf), Color);
+            _lines[6] = new Line(
+                new Vector3(_transform.Position.X + xHalf, _transform.Position.X + yHalf, _transform.Position.X + zHalf),
+                new Vector3(_transform.Position.X + xHalf, _transform.Position.X + yHalf, _transform.Position.X - zHalf), Color);
+            _lines[7] = new Line(
+                new Vector3(_transform.Position.X + xHalf, _transform.Position.X + yHalf, _transform.Position.X - zHalf),
+                new Vector3(_transform.Position.X - xHalf, _transform.Position.X + yHalf, _transform.Position.X - zHalf), Color);
+
+            //// vetical
+            _lines[8] = new Line(
+                new Vector3(_transform.Position.X - xHalf, _transform.Position.X - yHalf, _transform.Position.X - zHalf),
+                new Vector3(_transform.Position.X - xHalf, _transform.Position.X + yHalf, _transform.Position.X - zHalf), Color);
+            _lines[9] = new Line(
+                new Vector3(_transform.Position.X - xHalf, _transform.Position.X - yHalf, _transform.Position.X + zHalf),
+                new Vector3(_transform.Position.X - xHalf, _transform.Position.X + yHalf, _transform.Position.X + zHalf), Color);
+            _lines[10] = new Line(
+                new Vector3(_transform.Position.X + xHalf, _transform.Position.X - yHalf, _transform.Position.X + zHalf),
+                new Vector3(_transform.Position.X + xHalf, _transform.Position.X + yHalf, _transform.Position.X + zHalf), Color);
+            _lines[11] = new Line(
+                new Vector3(_transform.Position.X + xHalf, _transform.Position.X - yHalf, _transform.Position.X - zHalf),
+                new Vector3(_transform.Position.X + xHalf, _transform.Position.X + yHalf, _transform.Position.X + zHalf), Color);
+
+            NotifyPropertyChanged();
+        }
     }
 }
